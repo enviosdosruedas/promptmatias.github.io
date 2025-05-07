fd65ebdb928f990a2a4821033389bbfb045b9017
Envios Dosruedas <enviosdosruedas@gmail.com>
2025-05-07 05:55:54 +0000

    a

--- src/app/envios-emprendedores/page.tsx
+++ src/app/envios-emprendedores/page.tsx
@@ -6,9 +6,6 @@ import { Handshake, PackageSearch, BadgePercent } from 'lucide-react';
 import { BeneficiosEmprendedores } from '@/components/sections/BeneficiosEmprendedores'; // Import the new Hero component
 import { InfoEmprendedores } from '@/components/sections/info-emprendedores'; 
 import { PrecioEmprendedores } from '@/components/sections/precio-emprendedores'; 
-import { CompromisoEmprendedores } from '@/components/sections/compromiso-emprendedores'; 
-import { RecomendacionesEmprendedores } from '@/components/sections/recomendaciones-emprendedores'; 
-import { ListoEmprendedores } from '@/components/sections/listo-emprendedores'; 
 import { BannerRedesSociales } from '@/components/sections/banner-redes-sociales'; 
 
 export default function EnviosEmprendedoresPage() {
@@ -17,9 +14,6 @@ export default function EnviosEmprendedoresPage() {
       <BeneficiosEmprendedores /> {/* Add the Hero component here */}
       <InfoEmprendedores /> {/* Integrate the InfoEmprendedores component here */}
       <PrecioEmprendedores /> {/* Integrate the PrecioEmprendedores component here */}
-      <CompromisoEmprendedores /> {/* Integrate the CompromisoEmprendedores component here */}
-      <RecomendacionesEmprendedores /> {/* Integrate the new component */}
-      <ListoEmprendedores /> {/* Integrate the new ListoEmprendedores component here */}
        <BannerRedesSociales /> {/* Add banner at the end */}
     </>
   );
