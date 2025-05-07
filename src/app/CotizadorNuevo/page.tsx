163558dcb66d14038d07ea8ee8c9b5afc9a5896e
Envios Dosruedas <enviosdosruedas@gmail.com>
2025-05-07 06:35:56 +0000

    Necesito crear una pagina nueva en el directorio app/CotizadorNuevo/page.tsx que para iniciar tenga solamente el import { BannerRedesSociales } from '@/components/sections/banner-redes-sociales';

---
+++ src/app/CotizadorNuevo/page.tsx
@@ -0,0 +1,10 @@
+
+import { BannerRedesSociales } from '@/components/sections/banner-redes-sociales';
+
+export default function CotizadorNuevoPage() {
+  return (
+    <>
+      <BannerRedesSociales />
+    </>
+  );
+}
