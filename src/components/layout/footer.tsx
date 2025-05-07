bac127d34f1d26073df6d402e81f9fe95c7d3fbf
Envios Dosruedas <enviosdosruedas@gmail.com>
2025-05-07 07:04:03 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Footer>
    
    with the text content:
    
        Envios DosRuedasTu solución confiable para mensajería y delivery en Mar del Plata. MenúInicio ServiciosEnvíos ExpressEnvíos LowCostMoto ExpressMoto FijaPlan EmprendedoresEnvios Flex CotizarCotizar ExpressCotizar LowCost NosotrosSobre NosotrosPreguntas FrecuentesNuestras RedesContactoContacto  Mar del Plata, Argentina +54 223 660 2699 dosruedasmdq@gmail.comSíguenos en:   © 2025 Envios DosRuedas. Todos los derechos reservados.
    
    Relevant files:
    
        - https://6000-idx-studio-1746478522925.cluster-etsqrqvqyvd4erxx7qq32imrjk.cloudworkstations.dev/_next/static/chunks/791c6_next_dist_compiled_80a8e29a._.js
    
    And change it as follows:
    
    puedes hacer que los iconos los use desde la carpeta public/icons?

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -167,7 +167,7 @@ export function Footer() {
                 </a>
               </div>
               <div className="contact-item">
-                <Mail className="lucide" /> {/* Use Lucide Icon */}
+                <Image src="/icon/icon-gmail.svg" alt="Email" width={16} height={16} className="flex-shrink-0 mt-px" />
                 <a href="mailto:dosruedasmdq@gmail.com" className="whatsapp-link">dosruedasmdq@gmail.com</a>
               </div>
               <div className="social-container">
@@ -176,15 +176,13 @@ export function Footer() {
                 </h3>
                 <div className="social-links">
                   <Link href="https://instagram.com/enviosdosruedas" target="_blank" rel="noopener noreferrer" className="social-icon instagram" aria-label="Instagram">
-                     <Instagram className="lucide h-5 w-5" /> {/* Use Lucide Icon */}
+                     <Image src="/icon/icon-instagram.svg" alt="Instagram" width={20} height={20} />
                   </Link>
                   <Link href="https://facebook.com/enviosdosruedas" target="_blank" rel="noopener noreferrer" className="social-icon facebook" aria-label="Facebook">
-                    <Facebook className="lucide h-5 w-5" /> {/* Use Lucide Icon */}
+                    <Image src="/icon/icon-facebook.svg" alt="Facebook" width={20} height={20} />
                   </Link>
                   <Link href="https://wa.me/+542236602699?text=Hola!%20Encontre%20su%20contacto%20en%20el%20sitio%20web%20y%20me%20gustaria%20mas%20informacion%20sobre%20su%20servicio." target="_blank" rel="noopener noreferrer" className="social-icon whatsapp" aria-label="WhatsApp">
-                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.894 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 4.315 1.731 6.086l-.579 2.168 2.129-.565z" />
-                      </svg>
+                     <Image src="/icon/icon-whatsapp.svg" alt="WhatsApp" width={20} height={20} />
                   </Link>
                 </div>
               </div>
